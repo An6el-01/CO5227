bb51ef1d2b91745d23996468cc727186bb6200b0
Angel Salinas <a.m.salinas1901@gmail.com>
2023-01-17 14:03:22 +0000

    Add project files.

---
+++ AlfaBookStore/Model/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlfaBookStore.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string BookName { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+
+       //Next step is to load database on SQL look at the indian guy video minute 58:41
+       //Had to stop bc VPN for the school wont recognize that im connected to WI-FI
+    }
+}
